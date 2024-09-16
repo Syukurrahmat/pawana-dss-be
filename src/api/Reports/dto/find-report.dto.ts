496838bb67db13ac78a1a7b3ec14ed635e1dd74c
Syukurrahmat <syukurtullah@gmail.com>
2024-09-17 04:14:34 +0700

    add transformer

--- src/api/Reports/dto/find-report.dto.ts
+++ src/api/Reports/dto/find-report.dto.ts
@@ -1,15 +1,18 @@
-import { IsDate, IsDateString, IsIn, IsInt, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsDateString, IsIn, IsNumber, IsNumberString, IsOptional } from 'class-validator';
 
 export class FindReportDto {
     @IsDateString()
     @IsOptional()
     date?: Date;
 
-    @IsInt()
+    @IsNumber()
     @IsOptional()
+    @Type(() => Number) 
     nearCompany?: number;
 
-    @IsInt()
+    @IsNumber()
+    @Type(() => Number) 
     @IsIn([250, 500, 1000, 2000])
     @IsOptional()
     distance?: 500;
